a433dd727f64f869b519c16cbfbf4ece9608a4b9
Thabish <kadertabish2@gmail.com>
2023-06-19 17:00:13 +0400

    Create-update
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/app/dynamo/page.tsx
+++ src/app/dynamo/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import React from "react";
 import docClient from "@/app/dynamodb";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 const Dynamo = () => {
-	const dynamoCheck = () => {
+	const update = () => {
+		const createUserParmas = {
+			TableName: "stripe-customers",
+			Key: { id: "dddd" },
+			UpdateExpression: "SET isActive = :isActiveValue",
+			ExpressionAttributeValues: {
+				":isActiveValue": false,
+			},
+		};
+
+		return docClient.send(new UpdateCommand(createUserParmas));
+	};
+
+	const create = () => {
 		const createUserParmas = {
 			TableName: "stripe-customers",
 			Item: { id: "dddd", email: "test@email.com", isActive: true },
@@ -14,7 +27,8 @@ const Dynamo = () => {
 	return (
 		<div>
 			<h1>Clikc to check</h1>
-			<button onClick={dynamoCheck}>Check</button>
+			<button onClick={create}>Create</button>
+			<button onClick={update}>Update</button>
 		</div>
 	);
 };
